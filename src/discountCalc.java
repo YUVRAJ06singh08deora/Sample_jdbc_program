788dce059597078ecc62e66f9be73cb3d04e920c
YUVRAJ06singh08deora <06yuvraj2001singh@gmail.com>
2022-04-29 17:22:05 +0530

    All codes updated

--- src/discountCalc.java
+++ src/discountCalc.java
@@ -1,30 +1,37 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
 import java.util.Scanner;
 public class discountCalc extends JFrame {
     private JTextField shoppingAmt;
-    private JRadioButton visa;
-    private JRadioButton masterCard;
-    private JRadioButton rupay;
+    private JRadioButton cash;
+    private JRadioButton cheque;
+    private JRadioButton creditCard;
     private JTextField discount1;
     private JTextField netPrice1;
     private JButton clearAllButton;
     private JButton calculateNetPriceButton;
     private JButton exitButton;
     private JPanel shoppingMall;
-
+    private JTable table1;
+    private void createTablefromDatabase(String amount12,String cardType,String discount12,String netPrice15){
+        Object[][] data={
+                {amount12,cardType,discount12,netPrice15}
+        };
+        table1.setModel(new DefaultTableModel(
+                data,
+                new String[]{"Shopping Amount","Card Type","Discount","NetPrice"}
+        ));
+    }
     public discountCalc() {
         setTitle("Discount Form");
         setSize(450,350);
         setContentPane(shoppingMall);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
-//        ButtonGroup b1=new ButtonGroup();
-//        b1.add(visa);
-//        b1.add(masterCard);
-//        b1.add(rupay);
+
 
         exitButton.addActionListener(new ActionListener() {
             @Override
@@ -49,6 +56,7 @@ public class discountCalc extends JFrame {
             }
 
             private void calculateNetPrice() {
+
                 String shopingTxt = shoppingAmt.getText();
                 int amount= Integer.parseInt(shopingTxt);
                 float discount;
@@ -57,9 +65,26 @@ public class discountCalc extends JFrame {
                 String netPrice15;
                 String discount12;
                 String cardType;
-                if(visa.isSelected()) {
-                    System.out.println("Visa");
-                    if (amount < 5000) {
+
+                if(cash.isSelected()) {
+
+                    if (amount > 10000) {
+                        amount12=Float.toString(amount);
+                        discount = 0.2f * amount;
+                        discount1.setText(Float.toString(discount));
+                        discount12=Float.toString(discount);
+                        netPrice = amount-discount;
+                        netPrice1.setText(Float.toString(netPrice));
+                        netPrice15=Float.toString(netPrice);
+                        cardType = "Cash";
+                        addToDatabase(amount12,cardType,discount12,netPrice15);
+                        createTablefromDatabase(amount12,cardType,discount12,netPrice15);
+                    }
+
+                }
+                if(cheque.isSelected()){
+
+                    if (amount > 15000) {
                         amount12=Float.toString(amount);
                         discount = 0.1f * amount;
                         discount1.setText(Float.toString(discount));
@@ -67,46 +92,27 @@ public class discountCalc extends JFrame {
                         netPrice = amount-discount;
                         netPrice1.setText(Float.toString(netPrice));
                         netPrice15=Float.toString(netPrice);
-                        cardType = "Visa";
+                        cardType = "Cheque";
                         addToDatabase(amount12,cardType,discount12,netPrice15);
+                        createTablefromDatabase(amount12,cardType,discount12,netPrice15);
+                    }
 
-                    }
-                    if (amount >= 5000) {
-                        discount = 0.2f * amount;
-                        discount1.setText(Float.toString(discount));
-                        netPrice = amount-discount;
-                        netPrice1.setText(Float.toString(netPrice));
-                    }
                 }
-                if(masterCard.isSelected()){
-                    System.out.println("mastercard");
-                    if (amount < 10000) {
-                        discount = 0.15f * amount;
+                if(creditCard.isSelected()){
+
+                    if (amount > 10000) {
+                        amount12=Float.toString(amount);
+                        discount = 0.1f * amount;
                         discount1.setText(Float.toString(discount));
+                        discount12=Float.toString(discount);
                         netPrice = amount-discount;
                         netPrice1.setText(Float.toString(netPrice));
+                        netPrice15=Float.toString(netPrice);
+                        cardType = "creditCard";
+                        addToDatabase(amount12,cardType,discount12,netPrice15);
+                        createTablefromDatabase(amount12,cardType,discount12,netPrice15);
                     }
-                    if (amount >= 10000) {
-                        discount = 0.25f * amount;
-                        discount1.setText(Float.toString(discount));
-                        netPrice = amount-discount;
-                        netPrice1.setText(Float.toString(netPrice));
-                    }
-                }
-                if(rupay.isSelected()){
-                    System.out.println("Rupay");
-                    if (amount < 8000) {
-                        discount = 0.12f * amount;
-                        discount1.setText(Float.toString(discount));
-                        netPrice = amount-discount;
-                        netPrice1.setText(Float.toString(netPrice));
-                    }
-                    if (amount >= 8000) {
-                        discount = 0.15f * amount;
-                        discount1.setText(Float.toString(discount));
-                        netPrice = amount-discount;
-                        netPrice1.setText(Float.toString(netPrice));
-                    }
+
                 }
 
 
@@ -118,21 +124,21 @@ public class discountCalc extends JFrame {
         final String Db_URL="jdbc:mysql://localhost:3306/discount";
         final String USERNAME="root";
         final String PASSWORD="06yuvraj@SINGH";
-        Scanner k = new Scanner(System.in);
-        System.out.println("enter amount");
+
+
         String amount1 = amount;
-        System.out.println("enter discount");
+
         String discount1 = discount;
 
-        System.out.println("enter cardType");
+
         String cardType = CardType;
 
-        System.out.println("enter netprice");
+
         String netprice = netPrice;
 
         // Inserting data using SQL query
         String sql = "insert into amountDetails values('" + amount1
-                + "'," + discount1 + ",'" + cardType + "','"+ netprice +"')";
+                + "','" + cardType + "','" + discount1 + "','"+ netprice +"')";
 //
 //        // Connection class object
 //        Connection con = null;
